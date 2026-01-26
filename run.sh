895b9c58d0632b088d1ff3427f0c2fd7eadd2708
Siddhant <forsomethingnewsid@gmail.com>
2026-01-27 00:20:23 +0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
+apt-get update && xargs -a packages.txt apt-get install -y --no-install-recommends
 pip install playwright fastapi[all]
-playwright install chromium
-apt-get update && \
-    xargs -a packages.txt apt-get install -y --no-install-recommends && \
-    rm -rf /var/lib/apt/lists/*
+playwright install --with-deps
