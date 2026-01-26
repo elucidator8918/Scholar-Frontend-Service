79cce2c510eaf26111bdfc3b9823ff63f650bcd3
Siddhant <forsomethingnewsid@gmail.com>
2026-01-27 00:16:55 +0400

    Update Playwright installation to use Chromium

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 pip install playwright fastapi[all]
-playwright install --with-deps
+playwright install chromium
 apt-get update && \
     xargs -a packages.txt apt-get install -y --no-install-recommends && \
     rm -rf /var/lib/apt/lists/*
