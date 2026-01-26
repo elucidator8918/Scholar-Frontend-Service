103012d92914a089d96d0e34ee28f1a47c586f8f
Siddhant <forsomethingnewsid@gmail.com>
2026-01-26 19:39:43 +0000

    ok

--- run.sh
+++ run.sh
@@ -1,3 +1,18 @@
 pip install playwright fastapi[all]
 playwright install
 playwright install-deps
+apt-get update -q && \
+    apt-get install -y -qq --no-install-recommends \
+        xvfb \
+        libxcomposite1 \
+        libxdamage1 \
+        libatk1.0-0 \
+        libasound2 \
+        libdbus-1-3 \
+        libnspr4 \
+        libgbm1 \
+        libatk-bridge2.0-0 \
+        libcups2 \
+        libxkbcommon0 \
+        libatspi2.0-0 \
+        libnss3
